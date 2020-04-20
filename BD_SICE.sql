59c4607797e2b2cf1113b4843ecc9e21851b0e7d
cbrondon <35706801+cbrondon@users.noreply.github.com>
2020-04-19 22:27:07 -0500

    Update BD_SICE.sql
    Procedimientos almacenados que se agregaron posteriormente y correción al campo tecnico de la tabla Mantenimiento

--- BD_SICE.sql
+++ BD_SICE.sql
@@ -44,14 +44,14 @@ create table Departamento(
  foreign key (dpto) references Departamento(id)
  ) ;
  
--- drop table mantenimiento;
+-- drop table Mantenimiento;
 create table Mantenimiento(
-id varchar(5),
+id int not null auto_increment,
 tecnico varchar(5),
 equipo varchar(5),  
 fecha date,
 dpto varchar(5),
-primary key (id, tecnico,equipo, dpto),
+primary key (id,equipo, dpto),
 foreign key(tecnico) references Tecnico(num) on delete cascade on update cascade,
 foreign key(equipo) references Equipo(numinvent) on delete cascade on update cascade,
 foreign key(dpto) references Departamento(id) on delete cascade on update cascade
@@ -61,10 +61,10 @@ foreign key(dpto) references Departamento(id) on delete cascade on update cascad
   
 create table RealizaMntto(
 idtecnico varchar(5),
-idmntto varchar(5),
+idmntto int,
 idrealiza varchar(5),
 primary key (idtecnico, idmntto, idrealiza),
-foreign key (idmntto) references Mantenimiento (tecnico),
+foreign key (idmntto) references Mantenimiento (id),
 foreign key (idtecnico) references Tecnico (num)
 );
  
@@ -117,7 +117,8 @@ insert into Departamento(id, nomdpto, jefe) values ('1', 'sistemas', '87829'),
                                                                                                  ('19325',  'si','8557156945','8' , '256', 'amd', 'asus', '10'),
                                                                                                  ('24214',  'si','6848278518','4' , '128', 'intel' , 'hp', '5'),
 						                                                                         ('47036',  'no','3647478518','4' , '256', 'amd' , 'lenovo', '9');               
-   
+   insert into Mantenimiento(id,tecnico,equipo,fecha,dpto) values ('555', null, '47036','2020-04-18','9');
+
    insert into Mantenimiento(id, tecnico, equipo, fecha, dpto) values ('478', '84759','86075', '2020-04-15', '3'),
 																	  ('784', '41256', '12469', '2020-04-22', '7'),
                                                                       ('365', '36547', '47036','2020-04-18','9');
@@ -207,4 +208,38 @@ commit;
 end//
 
 delimiter ;
-							
+					
+
+delimiter //
+create procedure mostrarequipos(
+eq varchar(5),
+fech date,
+depto varchar(5),
+out mensaje varchar (30)
+)
+
+Begin
+
+ DECLARE EXIT HANDLER FOR SQLEXCEPTION
+ BEGIN 
+ SHOW ERRORS LIMIT 1;
+ ROLLBACK;
+ END; 
+ 
+ DECLARE EXIT HANDLER FOR SQLWARNING
+ BEGIN
+ SHOW WARNINGS LIMIT 1;
+ ROLLBACK;
+ END;
+
+START TRANSACTION;
+-- set @retVal = (select counT(*) FROM PARTICIPANTE WHERE teamid=id_e );
+
+   insert into mantenimiento(tecnico,equipo,fecha,dpto) values (null,eq,fech,depto);
+ -- insert into mantenimiento(tecnico,equipo,fecha,dpto) values (null, '25455','2020-04-29','1');
+ 
+commit;
+
+end//
+
+delimiter ;		
